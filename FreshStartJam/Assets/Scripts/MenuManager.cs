7c74845a51268267162c425199520b07cf90c198
Elizraa <56952777+Elizraa@users.noreply.github.com>
2021-03-01 06:34:46 +0700

    AddUninstall

--- FreshStartJam/Assets/Scripts/MenuManager.cs
+++ FreshStartJam/Assets/Scripts/MenuManager.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
     float dclick_threshold = 0.3f, timerdclick = 0;
 
-    public GameObject errorPanel, installPanel;
+    public GameObject errorPanel, LevelPanel, uninstallPanel, installPanel;
     public Animator animInstall;
 
+    public Button[] levelInstallButton, levelUninstallButton;
+    bool installOpened = true;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +20,10 @@ public class MenuManager : MonoBehaviour
         PlayerPrefs.DeleteAll();
         if (PlayerPrefs.HasKey("FirstOpen"))
         {
+            LevelPanel.SetActive(true);
             installPanel.SetActive(true);
         }
+        ChangeGreen();
     }
 
     // Update is called once per frame
@@ -45,7 +50,7 @@ public class MenuManager : MonoBehaviour
     public void DoFreshStart()
     {
         errorPanel.SetActive(false);
-        installPanel.SetActive(true);
+        LevelPanel.SetActive(true);
         animInstall.Play("firstReload");
         PlayerPrefs.SetInt("FirstOpen", 0);
     }
@@ -58,5 +63,43 @@ public class MenuManager : MonoBehaviour
     public void GoToLevel(int index)
     {
         SceneManager.LoadScene(index);
+    }
+
+    public void ChangePanel()
+    {
+        if (installOpened)
+        {
+            uninstallPanel.SetActive(true);
+            installPanel.SetActive(false);
+            installOpened = false;
+            return;
+        }
+        uninstallPanel.SetActive(false);
+        installPanel.SetActive(true);
+        installOpened = true;
+    }
+
+    void ChangeGreen()
+    {
+        for(int i = 0; i < levelInstallButton.Length; i++)
+        {
+            if (!PlayerPrefs.HasKey(levelInstallButton[i].gameObject.name))
+                break;
+            ColorBlock colors = levelInstallButton[i].colors;
+            Color temp = colors.normalColor;
+            temp.a = 0.5f;
+            colors.normalColor = temp;
+            levelInstallButton[i].colors = colors;
+        }
+        for (int i = 0; i < levelUninstallButton.Length; i++)
+        {
+            if (!PlayerPrefs.HasKey(levelUninstallButton[i].gameObject.name))
+                break;
+            ColorBlock colors = levelUninstallButton[i].colors;
+            Color temp = colors.normalColor;
+            temp.a = 0.5f;
+            colors.normalColor = temp;
+            levelUninstallButton[i].colors = colors;
+        }
     }
 }
