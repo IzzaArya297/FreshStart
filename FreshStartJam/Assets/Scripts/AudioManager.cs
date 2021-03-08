376d121b6cec8c05c08d8f5db860d4ee43b36a9d
Elizraa <56952777+Elizraa@users.noreply.github.com>
2021-03-08 13:32:09 +0700

    Add tutorial

---
+++ FreshStartJam/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager audioManager;
+
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(audioManager == null)
+        {
+            audioManager = this;
+            audioSource = GetComponent<AudioSource>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+
+}
